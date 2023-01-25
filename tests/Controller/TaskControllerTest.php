ba9dcc4ebb2816697d53c988180a5de677fbd2fa
armand <admindev@armand-selbmann.fr>
2023-01-25 11:00:20 +0100

    Update tests #119

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -63,6 +63,22 @@ class TaskControllerTest extends WebTestCase
 
     /**
      * @return void
+     * @throws Exception
+     */
+    public function testDisplayTaskManageList(): void
+    {
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testAdminUser = $userRepository->findOneByUsername('jane');
+        $this->client->loginUser($testAdminUser);
+
+        $this->client->request('GET', '/tasks/manage');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1',"Liste des tâches" );
+    }
+
+    /**
+     * @return void
      */
     public function testBackToTaskListFromTaskCreate(): void
     {
@@ -92,11 +108,28 @@ class TaskControllerTest extends WebTestCase
         );
     }
 
+    public function testTaskCreationDontWorking(): void
+    {
+        $crawler = $this->client->request('GET', '/tasks/create');
+
+        $form = $crawler->selectButton('Ajouter')->form();
+        $form['task[title]'] = 'Tâche pour erreur';
+        $form['task[content]'] = '';
+        $this->client->submit($form);
+
+        $this->assertSelectorNotExists(
+            'div.alert.alert-success',
+            'La tâche a été bien été ajoutée'
+        );
+    }
+
+
+
     /**
      * @return void
      * @throws Exception
      */
-    public function testTaskEditIsDone(): void
+    public function testTaskEditIsDoneByRegisteredUser(): void
     {
         $taskRepository = static::getContainer()->get(TaskRepository::class);
         $task = $taskRepository->findBy(array('user' => '1', 'isDone' => '1'), array(), 1);
@@ -109,6 +142,66 @@ class TaskControllerTest extends WebTestCase
         $form = $crawler->selectButton('Modifier')->form();
         $form['task[title]'] = 'Tâche pour un test de modification';
         $form['task[content]'] = 'Un contenu de test de modification';
+        $this->client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertSelectorTextContains(
+            'div.alert.alert-success',
+            'Superbe ! La tâche a bien été modifiée.'
+        );
+    }
+
+    /**
+     * @return void
+     * @throws Exception
+     */
+    public function testAnonymousTaskEditByAdmin(): void
+    {
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testAdminUser = $userRepository->findOneByUsername('jane');
+        $this->client->loginUser($testAdminUser);
+
+        $taskRepository = static::getContainer()->get(TaskRepository::class);
+        $task = $taskRepository->findBy(array('user' => null), array(), 1);
+        $task = $taskRepository->find($task[0]->getId());
+        $taskId = $task->getId();
+
+
+        $crawler = $this->client->request('GET', "/tasks/$taskId/edit");
+
+        $form = $crawler->selectButton('Modifier')->form();
+        $form['task[title]'] = 'Tâche pour un test de modification par un administrateur';
+        $form['task[content]'] = 'Un contenu de test de modification par un administrateur';
+        $this->client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertSelectorTextContains(
+            'div.alert.alert-success',
+            'Superbe ! La tâche a bien été modifiée.'
+        );
+    }
+
+    /**
+     * @return void
+     * @throws Exception
+     */
+    public function testOwnTaskEditByAdmin(): void
+    {
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testAdminUser = $userRepository->findOneByUsername('jane');
+        $this->client->loginUser($testAdminUser);
+
+        $taskRepository = static::getContainer()->get(TaskRepository::class);
+        $task = $taskRepository->findBy(array('user' => $testAdminUser), array(), 1);
+        $task = $taskRepository->find($task[0]->getId());
+        $taskId = $task->getId();
+
+
+        $crawler = $this->client->request('GET', "/tasks/$taskId/edit");
+
+        $form = $crawler->selectButton('Modifier')->form();
+        $form['task[title]'] = 'Tâche pour un test de modification par un administrateur';
+        $form['task[content]'] = 'Un contenu de test de modification par un administrateur';
         $this->client->submit($form);
 
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
@@ -231,8 +324,8 @@ class TaskControllerTest extends WebTestCase
     public function testTaskDeleteIsProhibitedForThisUser(): void
     {
         $userRepository = static::getContainer()->get(UserRepository::class);
-        $testUser = $userRepository->findOneByUsername('lolo');
-        $this->client->loginUser($testUser);
+        $testRegisteredUser = $userRepository->findOneByUsername('lolo');
+        $this->client->loginUser($testRegisteredUser);
 
         $taskRepository = static::getContainer()->get(TaskRepository::class);
         $task = $taskRepository->findBy(array('user' => '2'), array(), 1);
@@ -242,6 +335,27 @@ class TaskControllerTest extends WebTestCase
         $this->assertSelectorTextContains(
             'div.alert.alert-danger',
             'Oops ! Vous ne pouvez pas supprimer cette tâche.'
+        );
+    }
+
+    /**
+     * @return void
+     * @throws Exception
+     */
+    public function testAnonymousTaskDeleteByAdmin(): void
+    {
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testAdminUser = $userRepository->findOneByUsername('jane');
+        $this->client->loginUser($testAdminUser);
+
+        $taskRepository = static::getContainer()->get(TaskRepository::class);
+        $task = $taskRepository->findBy(array('user' => null), array(), 1);
+        $taskId = $task[0]->getId();
+        $this->client->request('GET', "/tasks/$taskId/delete");
+
+        $this->assertSelectorTextContains(
+            'div.alert.alert-success',
+            'La tâche a bien été supprimée.'
         );
     }
 
